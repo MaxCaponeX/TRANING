85a5c13b4d00ce11988fb93d3dacfe951c6443dc
Maksym Poberegnyi <pobermaks@gmail.com>
2023-04-20 00:33:55 +0300

    index.js new file

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// let min = 0;
+
+// for (let i = 0; i < arr; i++) {
+//    console.log(arr[i]);
+//    for (let j = 1; j < arr; j++) {
+//       if (arr[i] < arr[j]) return (min = arr[i]);
+//    }
+// }
+
+console.log("hello");
